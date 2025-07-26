8cc540156fd4791a82a686ecc7df966d3ebf908e
kadam_sk <157714727+Sharadkadam45@users.noreply.github.com>
2025-07-26 23:39:32 +0530

    Time: 0 ms (100%), Space: 12.1 MB (85.06%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool helper(vector<int>& citations,int h){
+        int n=citations.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(citations[i]>=h) cnt++;
+        }
+        if(cnt>=h) return true;
+        else return false;
+    }
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        if(n==1){
+            if(citations[0]>0) return 1;
+            return 0;
+        }
+        int low=1;
+        int high=INT_MIN;
+
+        for(int i=0;i<n;i++){
+            // low=min(low,citations[i]);
+            high=max(high,citations[i]);
+        }
+
+        int  ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(helper(citations,mid)){
+                ans=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return ans;
+    }
+};
