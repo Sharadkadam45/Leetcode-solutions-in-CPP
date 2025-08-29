1d1f9dc2f161782d63b2a8c2fdca436ce7857b68
kadam_sk <157714727+Sharadkadam45@users.noreply.github.com>
2025-08-29 15:08:51 +0530

    Time: 27 ms (12.36%), Space: 17 MB (9.21%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    int max(int a,int b){
-        if(a>=b) return a;
-        return b;
-    }
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
-        int j=0;
         int i=0;
-        unordered_set<char> st;
-        int maxx=INT_MIN;
-        while(j<n){
+        int j=0;
+        set<char> st;
+        int ans=0;
+        while(j<n){ 
             if(st.find(s[j])!=st.end()){
-                // shrink the window
-                while(s[j]!=s[i]){
+                // cout<<j<<endl;
+                while(i<n && s[j]!=s[i]){
                     st.erase(s[i]);
                     i++;
                 }
                 st.erase(s[i]);
                 i++;
             }
-            else {
+            else{
                 st.insert(s[j]);
+                ans=max(ans,j-i+1);
                 j++;
-            }
-            maxx=max(maxx,st.size());
+            } 
         }
-        if(maxx==INT_MIN) return 0;
-        return maxx;
+        return ans;
     }
 };
