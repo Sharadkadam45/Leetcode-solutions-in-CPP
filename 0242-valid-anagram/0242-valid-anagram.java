1fa1f6980d81684f4a50032fc207069318b779cc
kadam_sk <157714727+Sharadkadam45@users.noreply.github.com>
2026-01-04 19:05:30 +0530

    Time: 21 ms (9.71%), Space: 46.6 MB (24.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-
         Map<Character,Integer> mp=new HashMap<>();
+        int n=s.length();
 
-        char[] c1=s.toCharArray();
-        char[] c2=t.toCharArray();
+        char[] s1=s.toCharArray();
+        char[] s2=t.toCharArray();
 
-        for(char ch:c1){
-            mp.put(ch,mp.getOrDefault(ch,0)+1);
+        for(int i=0;i<n;i++){
+            mp.put(s1[i],mp.getOrDefault(s1[i],0)+1);
         }
 
-        for(char ch:c2){
-            if(mp.containsKey(ch)){
-                mp.put(ch,mp.get(ch)-1);
-                if(mp.get(ch)==0){
-                    mp.remove(ch);
-                }
-            }
-            else return false;
+        for(int i=0;i<n;i++){
+            if(!mp.containsKey(s2[i])) return false;
+            mp.put(s2[i],mp.get(s2[i])-1);
+            if(mp.get(s2[i])==0) mp.remove(s2[i]);
         }
-
         return (mp.size()==0);
     }
 }
